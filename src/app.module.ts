9b7f6b0b1e1eead158cfd1dcfb5d7d2a22d93ba4
mxrcury <demetrgonchar@gmail.com>
2023-05-15 21:11:30 +0300

    create config dir for configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,14 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserEntity } from 'src/entities/user.entity'
-import { AuthModule } from 'src/auth/auth.module'
+import { AuthModule } from 'src/auth/auth.module';
+import { envOptions, ormOptions } from './configs';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env.local',
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: parseInt(configService.get('DATABASE_PORT')),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [UserEntity],
-        logging: false,
-      })
-    }),
+    ConfigModule.forRoot(envOptions),
+    TypeOrmModule.forRootAsync(ormOptions),
     AuthModule,
   ],
 })
