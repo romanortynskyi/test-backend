0db5f9d27df0f6933ef1962105ce4ed6a0541f96
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2023-05-20 00:03:03 +0300

    fix

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -3,9 +3,9 @@ import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 
 import { CreateCashflow1684400984857 } from './migrations/1684400984857-CreateCashflow';
-import { CashflowEntity } from 'src/entities/cashflow.entity';
-import { CreditPaymentEntity } from 'src/entities/credit-payment.entity';
-import { CreditEntity } from 'src/entities/credit.entity';
+import { CashflowEntity } from './src/entities/cashflow.entity';
+import { CreditPaymentEntity } from './src/entities/credit-payment.entity';
+import { CreditEntity } from './src/entities/credit.entity';
 import { CreateUser1683525459189 } from './migrations/1683525459189-CreateUser';
 import { AddUserImage1684060336867 } from './migrations/1684060336867-AddUserImage';
 import { AddUserRecoveryCode1684327028472 } from './migrations/1684327028472-AddUserRecoveryCode';
